b407c00a9b870ca7a01129564d982f9986c6479f
Ian Sarasty Medina <ian.sarasty@skaphe.com>
2014-03-24 20:42:42 -0500

    nueva estructura y planteamento

--- src/puzzle/Arbol.java
+++ src/puzzle/Arbol.java
@@ -1,16 +1,37 @@
 package puzzle;
 
-public class Arbol<T> {
-    private static final int SIZE = 9;
+import java.util.Stack;
+
+public class Arbol {
+    private static int SIZE;
     
     private static String PROFUNDIDAD = "PROFUNDIDAD";
     private static String ITERATIVO = "ITERATIVO";
     private static String AMPLITUD = "AMPLITUD";
     
-    private Nodo raiz;    
+    private Nodo raiz;   
+    private Hash nuestroHash = null;
+    private Stack stack = new Stack();
+    
+    private int[] tablero = null;
+    
+    public Arbol (int keyNodo, int[] tablero){
+        raiz = new Nodo();
+        raiz.key = keyNodo;
+        nuestroHash = new Hash();
+        this.tablero = tablero;
+        SIZE = tablero.length;        
+    }
     
     public void insertarProfundidad (Nodo keyNodo){
         
+        Nodo actual;
+        stack.add(keyNodo);
+        
+        while(!stack.empty() && keyNodo.key != 123456789){
+            actual =(Nodo) stack.pop();
+            generarEstadoNodoNoDuplicado(actual, PROFUNDIDAD);            
+        }
     }
     public void insertarIterativo (Nodo keyNodo){
         
@@ -19,7 +40,7 @@ public class Arbol<T> {
         
     }
     
-    private void addHijo(Nodo hijo, int[] tablero, String tipoInsercion){
+    private void addHijo(Nodo hijo, String tipoInsercion){
         switch(tipoInsercion){
             case "PROFUNDIDAD":
                     insertarProfundidad(hijo);
@@ -33,139 +54,113 @@ public class Arbol<T> {
         }
     }
     
-    
-    private boolean esMismoEstado(int [] tablero, Nodo nodo){
-        return false;
-    }
-    
-    private boolean esMismoEstado(int [] tablero, int key){
-        return false;
+    private boolean esMismoEstado(int key){
+        if(nuestroHash.buscar(key) == null){
+            return false;
+        }
+        return true;
     }
     
     private int getPosicion(Nodo nodo){
         return nodo.getPosVacia();
     }
     
-    private void nuevoEstado(int [] tablero, int estadoActual, int posicion, int nuevaPosicion){
+    private int nuevoEstado(int [] tablero, int estadoActual, int posicion, int nuevaPosicion){
+        int aux = tablero[posicion];        
+        tablero[posicion] = tablero[nuevaPosicion];
+        tablero[nuevaPosicion] = aux;
+        String stringEstado = Integer.toString(tablero[0]) ;
+        for (int i=1; i < SIZE; i++) {
+            stringEstado = stringEstado + tablero[i];
+        }        
+        return Integer.parseInt(stringEstado) ;
     }
     
-    private boolean generarEstadoNodoNoDuplicado(Nodo nodoActual, String tipoInsercion){
+    private void generarEstadoNodoNoDuplicado(Nodo nodoActual, String tipoInsercion){
         int posicion;
         int estadoActual;
-        int[] tablero = new int[SIZE];
-        
+                
         estadoActual = (int) nodoActual.key;                    //es el estado del tablero
         posicion = getPosicion(nodoActual);                     //este metodo buscara en que posicion esta el simbolo de vacio
         
         if( (posicion-3)>=0 ){
-            nuevoEstado(tablero, estadoActual, posicion, posicion - 3);        //          aca se agrega un nuevo estado en la hash
-            if(nodoActual.padre == null){
-                addHijo(nodoActual, tablero, tipoInsercion);
-            }
-            else{
-                if(!esMismoEstado(tablero, nodoActual)){            //         se revisa si es un nuevo estado y se crea un hijo
-                    addHijo(nodoActual, tablero, tipoInsercion);
-                }
-            }
+            if(!esMismoEstado(nuevoEstado(tablero, estadoActual, posicion, posicion - 3))){   //se revisa si es un nuevo estado y se crea un hijo
+                addHijo(nodoActual, tipoInsercion);
+            }            
         }
 
         if( (posicion-1)>=0 && (posicion-1)!=2 && (posicion-1)!=5){
-            nuevoEstado(tablero, estadoActual, posicion, posicion - 1);        //          aca se agrega un nuevo estado en la hash
-            if(nodoActual.padre==null){ 
-                addHijo(nodoActual, tablero, tipoInsercion);
-            }
-            else{
-                if(!esMismoEstado(tablero, nodoActual)){ 
-                    addHijo(nodoActual, tablero, tipoInsercion);
-                }
+            if(!esMismoEstado(nuevoEstado(tablero, estadoActual, posicion, posicion - 1))){ 
+                    addHijo(nodoActual, tipoInsercion);
             }           
         }
 
         if( (posicion+3)<SIZE ){
-            nuevoEstado(tablero, estadoActual, posicion, posicion + 3);        //          aca se agrega un nuevo estado en la hash
-            if(nodoActual.padre == null){ 
-                addHijo(nodoActual, tablero, tipoInsercion);
-            }
-            else{
-                if(!esMismoEstado(tablero, nodoActual)){ 
-                    addHijo(nodoActual, tablero, tipoInsercion);
-                }
+            if(!esMismoEstado(nuevoEstado(tablero, estadoActual, posicion, posicion + 3))){ 
+                    addHijo(nodoActual, tipoInsercion);
             }
         }
 
         if( (posicion+1)<SIZE && (posicion+1)!=3 && (posicion+1)!=6){
-            nuevoEstado(tablero, estadoActual, posicion, posicion +1);        //          aca se agrega un nuevo estado en la hash
-            if(nodoActual.padre == null){ 
-                addHijo(nodoActual, tablero, tipoInsercion);
-            }
-            else{
-                if(!esMismoEstado(tablero, nodoActual)){
-                    addHijo(nodoActual, tablero, tipoInsercion); 
-                }
+            if(!esMismoEstado(nuevoEstado(tablero, estadoActual, posicion, posicion +1))){
+                    addHijo(nodoActual, tipoInsercion); 
             }
         }
-        return false;
+//        return false;
     }
     
-    private boolean generarEstadoNodo(Nodo nodoActual, String tipoInsercion){
-        int posicion;
-        int estadoActual;
-        int[] tablero = new int[SIZE];
-        
-        estadoActual = (int) nodoActual.key;                    //es el estado del tablero
-        posicion = getPosicion(nodoActual);                     //este metodo buscara en que posicion esta el simbolo de vacio
-        
-        if( (posicion-3)>=0 ){
-            nuevoEstado(tablero, estadoActual, posicion, posicion-3);
-            if(nodoActual.padre == null){
-                addHijo(nodoActual, tablero, tipoInsercion);
-            }
-            else{
-                if(!esMismoEstado(tablero, nodoActual.padre.key)){
-                    addHijo(nodoActual, tablero, tipoInsercion);
-                }
-            }
-        }
-        if( (posicion-1)>=0 && (posicion-1)!=2 && (posicion-1)!=5){
-            nuevoEstado(tablero, estadoActual, posicion, posicion-1);
-            if(nodoActual.padre == null){
-                addHijo(nodoActual, tablero, tipoInsercion);
-            }
-            else{
-                if(!esMismoEstado(tablero, nodoActual.padre.key)){
-                    addHijo(nodoActual, tablero, tipoInsercion);
-                }
-            }
-        }
-        if( (posicion+3)<SIZE ){
-            nuevoEstado(tablero, estadoActual, posicion, posicion+3);
-            if(nodoActual.padre == null){ 
-                addHijo(nodoActual, tablero, tipoInsercion);
-            }
-            else{
-                if(!esMismoEstado(tablero, nodoActual.padre.key)){ 
-                    addHijo(nodoActual, tablero, tipoInsercion);
-                }
-            }
-        }
-        if( (posicion+1)<SIZE && (posicion+1)!=3 && (posicion+1)!=6){
-            nuevoEstado(tablero, estadoActual, posicion, posicion+1);
-            if(nodoActual.padre == null){ 
-                addHijo(nodoActual, tablero, tipoInsercion);
-            }
-            else{
-                if(!esMismoEstado(tablero, nodoActual.padre.key)){
-                    addHijo(nodoActual, tablero, tipoInsercion);
-                }
-            }
-        }
-        return false;
-    }
-    
-    
-    public Arbol (int keyNodo){
-        raiz = new Nodo();
-        raiz.key = keyNodo;
-    }
-    
+//    private boolean generarEstadoNodo(Nodo nodoActual, String tipoInsercion){
+//        int posicion;
+//        int estadoActual;
+//        int[] tablero = new int[SIZE];
+//        
+//        estadoActual = (int) nodoActual.key;                    //es el estado del tablero
+//        posicion = getPosicion(nodoActual);                     //este metodo buscara en que posicion esta el simbolo de vacio
+//        
+//        if( (posicion-3)>=0 ){
+//            nuevoEstado(tablero, estadoActual, posicion, posicion-3);
+//            if(nodoActual.padre == null){
+//                addHijo(nodoActual, tablero, tipoInsercion);
+//            }
+//            else{
+//                if(!esMismoEstado(tablero, nodoActual.padre.key)){
+//                    addHijo(nodoActual, tablero, tipoInsercion);
+//                }
+//            }
+//        }
+//        if( (posicion-1)>=0 && (posicion-1)!=2 && (posicion-1)!=5){
+//            nuevoEstado(tablero, estadoActual, posicion, posicion-1);
+//            if(nodoActual.padre == null){
+//                addHijo(nodoActual, tablero, tipoInsercion);
+//            }
+//            else{
+//                if(!esMismoEstado(tablero, nodoActual.padre.key)){
+//                    addHijo(nodoActual, tablero, tipoInsercion);
+//                }
+//            }
+//        }
+//        if( (posicion+3)<SIZE ){
+//            nuevoEstado(tablero, estadoActual, posicion, posicion+3);
+//            if(nodoActual.padre == null){ 
+//                addHijo(nodoActual, tablero, tipoInsercion);
+//            }
+//            else{
+//                if(!esMismoEstado(tablero, nodoActual.padre.key)){ 
+//                    addHijo(nodoActual, tablero, tipoInsercion);
+//                }
+//            }
+//        }
+//        if( (posicion+1)<SIZE && (posicion+1)!=3 && (posicion+1)!=6){
+//            nuevoEstado(tablero, estadoActual, posicion, posicion+1);
+//            if(nodoActual.padre == null){ 
+//                addHijo(nodoActual, tablero, tipoInsercion);
+//            }
+//            else{
+//                if(!esMismoEstado(tablero, nodoActual.padre.key)){
+//                    addHijo(nodoActual, tablero, tipoInsercion);
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
